6fc453dd8f20f00f51453f3421906a64c5bf5eba
duckgii <shinyonggi@naver.com>
2024-09-14 22:29:14 +0900

    fix : leaks

--- 3. 데코레이터 패턴/main.cpp
+++ 3. 데코레이터 패턴/main.cpp
@@ -10,11 +10,23 @@ int main()
 {
 	Beverage *beverage = new Espresso();
 	Beverage *beverage2 = new HouseBlend();
+	Beverage *temp1;
+	Beverage *temp2;
+	Beverage *temp3;
 	atexit(check);
 
 	cout << beverage->getDescription() << " $" << beverage->cost() << endl;
+	temp1 = beverage2;
 	beverage2 = new Soy(beverage2);
+	temp2 = beverage2;
 	beverage2 = new Mocha(beverage2);
+	temp3 = beverage2;
 	beverage2 = new Whip(beverage2);
 	cout << beverage2->getDescription() << " $" << beverage2->cost() <<endl;
+	
+	delete beverage2;
+	delete temp1;
+	delete temp2;
+	delete temp3;
+	delete beverage;
 }
